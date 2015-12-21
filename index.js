75cd54bab27099a139a12b01f958b052cc6b6024
Hao-kang Den <haokang.den@gmail.com>
2015-12-21 16:03:51 +0800

    allow custom emitter
    - allow custom emitter
    - support socket.io client

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 'use strict'
-var Emitter = require('events').EventEmitter
-var emit = Emitter.prototype.emit
+var BuiltInEmitter = require('events').EventEmitter
 
-function onevent (packet) {
-  var args
-  args = packet.data || []
-  if (packet.id != null) {
-    args.push(this.ack(packet.id))
+module.exports = function (CustomEmitter) {
+  var Emitter = CustomEmitter || BuiltInEmitter
+  var emit = Emitter.prototype.emit
+
+  function onevent (packet) {
+    var args = packet.data || []
+    if (packet.id != null) {
+      args.push(this.ack(packet.id))
+    }
+    emit.call(this, '*', packet)
+    return emit.apply(this, args)
   }
-  emit.call(this, '*', packet)
-  return emit.apply(this, args)
-}
 
-module.exports = function () {
   return function (socket, next) {
     if (socket.onevent !== onevent) {
       socket.onevent = onevent
     }
-    return next()
+    return next ? next() : null
   }
 }
